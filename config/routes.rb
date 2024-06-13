22b9dab98adc477f93544b9683b73498e1ef9f73
yyudaii <yudai200009089311@gmail.com>
2024-06-13 17:49:01 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   #root "articles#index"
   root to: "rooms#index"
   resources :users, only:[:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index,:create]
   end
 end
